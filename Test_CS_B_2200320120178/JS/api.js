8d90843e5b455d1e912d591bb1bbb2f6cf420451
Shubham Choudhary <shubhamchoudhary.030704@gmail.com>
2024-11-08 19:32:55 +0530

    added some feature

--- Test_CS_B_2200320120178/JS/api.js
+++ Test_CS_B_2200320120178/JS/api.js
@@ -3,9 +3,9 @@ const weather = async () => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},IN&appid=96e90262d1a0563740a9a1f7f601a74c&units=metric`)
     const data = await response.json()
     console.log(data)
-    document.getElementById('cityname').innerText= data.name
-    document.getElementById('min').innerText= data.main.temp_min
-    document.getElementById('max').innerText=data.main.temp_max
-    document.getElementById('hum').innerText=data.main.humidity
-    document.getElementById('wind').innerText=data.wind.speed
+    document.getElementById('cityname').innerHTML= data.name
+    document.getElementById('min').innerHTML= data.main.temp_min
+    document.getElementById('max').innerHTML=data.main.temp_max
+    document.getElementById('hum').innerHTML=data.main.humidity
+    document.getElementById('wind').innerHTML=data.wind.speed
 }
